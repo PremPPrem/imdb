1168d48c066fd436102884d0f6ca585a9466fee5
PremPPrem <onusoka.thaika@gmail.com>
2023-06-20 01:54:48 +0700

    clear
    clear project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import "@/styles/globals.scss";
+import Head from "next/head";
+import Layout from "./component/Layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>IMDB Clone</title>
+        <meta name="description" content="imdb" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
