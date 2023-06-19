3190150755df121b2152d377cdaa5fe88d3697c1
PremPPrem <onusoka.thaika@gmail.com>
2023-06-20 04:20:02 +0700

    about page

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nam, error
+        illum ipsam commodi explicabo! Nam hic perspiciatis sint, eaque numquam,
+        id, ullam aperiam officiis repellat dicta a unde eligendi harum. Optio
+        veritatis tenetur voluptas?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, labore
+        at molestiae saepe cum alias aliquam excepturi. Beatae reiciendis
+        aliquam vitae quisquam exercitationem corrupti quaerat autem ad nemo ex
+        facilis culpa cupiditate aspernatur, temporibus architecto, vero
+        recusandae suscipit ipsum laudantium error dolor! Nostrum, ipsa
+        similique!
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+        harum itaque laudantium commodi provident molestiae fugiat ullam ex sed
+        similique. Libero, animi possimus, veniam minus neque facilis obcaecati
+        quam veritatis excepturi ab voluptate deleniti fuga eum beatae tenetur
+        dolorum cum earum quos porro! Beatae harum voluptas dignissimos eaque
+        facere, voluptatum blanditiis tempore quidem porro est maiores incidunt
+        odio, repellat animi?
+      </p>
+    </div>
+  );
+}
